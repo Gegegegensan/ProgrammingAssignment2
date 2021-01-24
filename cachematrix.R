0e89dbbec4508584e5821ddb598597cdd8b7b065
Ryo <Gegegegensan@users.noreply.github.com>
2021-01-24 22:44:43 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment: Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## This function creates getters of the matrix and inverse matrix and 
+## setters of the matrix and inverse matrix that are accessible from outside of the function
+## The inverse is returned from from the cache after running cacheSolve() function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+  inverse <- NULL
+  
   set <- function(y) {
     x <<- y
-    m <<-NULL
+    inverse <<-NULL
   }
+  
   get <- function() x
-  setmatrix <- function(matrix) m <<- meatrix
-  getmatrix <- function() m
+  
+  setinversematrix <- function(solve) inverse <<- solve
+  getinversematrix <- function() inverse
+  
   list(set = set, get = get,
-       setmatrix = setmatrix,
-       getmatrix = getmatrix)
-
+       setinversematrix = setinversematrix,
+       getinversematrix = getinversematrix)
 }
 
 
-## Write a short comment describing this function
+## This function calculates inverse matrix and set the calculated value onto makeCacheMatrix() function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  m <- x$getmatrix()
-  if(!is.null(m)) {
+  ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinversematrix()
+  if(!is.null(inverse)) {
     message("getting cached data")
-    return(m)
+    return(inverse)
   }
   data <- x$get()
-  m <- matrix(data, ...)
-  x$setmatrix(m)
-  m
+  inverse <- solve(data, ...)
+  x$setinversematrix(inverse)
+  inverse
 }
+
+
+## TEST 1
+test_matrix1 <- makeCacheMatrix(matrix(1001:1004, 2, 2))
+test_matrix1$get()
+test_matrix1$getinversematrix() ## should be NULL
+cacheSolve(test_matrix1)
+test_matrix1$getinversematrix() ## Should return inverse matrix
+
+#TEST 2
+test_matrix2 <- makeCacheMatrix(matrix(c(500, 4000, 456, 555), 2, 2))
+test_matrix2$get()
+test_matrix2$getinversematrix()
+cacheSolve(test_matrix2)
+test_matrix2$getinversematrix()
